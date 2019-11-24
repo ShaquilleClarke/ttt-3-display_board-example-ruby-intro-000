7e22a0dbe29ba5874bc3bb554428e31e37c1305b
Shaquille Clarke <shaqstaysfresh@gmail.com>
2019-11-24 14:51:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,3 @@
-def display_board(board)
-   " #{board[0]} | #{board[1]} | #{board[2]} "
-   "-----------"
-   " #{board[3]} | #{board[4]} | #{board[5]} "
-   "-----------"
-   " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+def display_board
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-display_board(board)
-display_board(board)
-display_board(board) # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+end # Define a method display_board that prints a 3x3 Tic Tac Toe Board
